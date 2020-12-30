6246733cd2c2d4fa596481f68bb43f76eb6343d0
moshe vilner <shootermv@gmail.com>
2020-12-30 07:55:17 +0200

    adding "id" field to all keyvalue pairs

--- src/api/stocksMock.js
+++ src/api/stocksMock.js
@@ -4,27 +4,27 @@ export const STOCKS = [
     name: "ForeScout Techno",
     mnumber: 25,
     keyVals: [
-      { key: "1", val: "1" },
-      { key: "sass", val: "sass" },
-      { key: "sss", val: "sss" }
+      { id: "11", key: "1", val: "1" },
+      { id: "12",key: "sass", val: "sass" },
+      { id: "13",key: "sss", val: "sss" }
     ]
   },
   {
     id: "AAPL",
     name: "Apple Computers",
     mnumber: 166,
-    keyVals: [{ key: "sss", val: "sss" }]
+    keyVals: [{ id: "21", key: "sss", val: "sss" }]
   },
   {
     id: "WIX",
     name: "Wix",
     mnumber: 69,
-    keyVals: [{ key: "aaa", val: "aaa" }]
+    keyVals: [{ id: "31", key: "aaa", val: "aaa" }]
   },
   {
     id: "MBLY",
     name: "Mobileye",
     mnumber: 62,
-    keyVals: [{ key: "bbb", val: "bbb" }]
+    keyVals: [{ id: "41", key: "bbb", val: "bbb" }]
   }
 ];
